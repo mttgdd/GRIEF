764bc16bec53929334a9fc090a0df25a3f36f43e
Matt Gadd <mattgadd@robots.ox.ac.uk>
2019-12-08 13:15:22 +0000

    pass mac os compilation option through scripts

--- tools/generate.sh
+++ tools/generate.sh
@@ -2,6 +2,6 @@ for i in 16 32 64;
 do ./grief/generate_code.py grief/test_pairs.txt $i >grief/generated_$i.i;
 done
 rm evolve_grief 
-make evolve_grief
+make MACOS=yes evolve_grief
 rm match_all
-make match_all 
+make MACOS=yes match_all 
